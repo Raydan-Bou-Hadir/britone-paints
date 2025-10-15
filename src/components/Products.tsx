57e53f5cb9e3fcc6e4e5bdfca8a53860120af7be
Raydan Bou hadir <raydanbouhadir00@gmail.com>
2025-10-16 00:37:50 +0300

    edit the buttons of product section

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useProductGroupStore } from "../store.ts/ProductGroup";
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
+import { LuSparkles } from "react-icons/lu";
 
 function Products() {
   const productGroup = useProductGroupStore((state) => state.productsGroups);
@@ -23,17 +24,20 @@ function Products() {
                 <h2 className="header">Our Products</h2>
                 <p className="sub-header">Discover our comprehensive range of high-quality paint solutions</p>
             </div>
-            <div className="flex flex-col justify-center items-center gap-6">
-                <h3 className="container-header">Featured Products</h3>
-                <div className="flex flex-row justify-center gap-6 w-[75%]">
+            <div className="w-[80%] mx-auto flex flex-col justify-center items-center gap-10">
+                <div className="flex flex-row gap-2 items-center self-start py-2 px-4 bg-gradient-to-br from-[#000068] via-[#680028] to-[#960000] rounded-4xl">
+                    <LuSparkles size={20} className="text-white"/>
+                    <h3 className="text-lg font-bold leading-relaxed text-white tracking-wide">Featured Products</h3>
+                </div>
+                <div className="flex flex-row justify-center items-center gap-10">
                     <button
                         onClick={handlePrev}
-                        className="navy-blue hover:text-[#960000] cursor-pointer disabled:opacity-50 disabled:cursor-default disabled:text-[#000000]"
+                        className="navy-blue hover:text-white bg-[#960000] cursor-pointer disabled:opacity-50 disabled:cursor-default disabled:text-[#000000]"
                         disabled={startIndex === 0}
                         >
                         <MdKeyboardArrowLeft size={32} className="border border-zinc-400 rounded bg-white shadow-sm"/>
                     </button>
-                    <div className="grid grid-cols-4 gap-6">
+                    <div className="grid grid-cols-4 gap-8">
                         {visibleGroups.map((item) => (
                             <div className="product-card hover:shadow-lg transition-shadow duration-300 cursor-pointer">
                                 <img src={item.productImage} alt={item.productName} className="w-full h-60 rounded-lg object-cover" />
@@ -43,10 +47,10 @@ function Products() {
                     </div>
                     <button
                         onClick={handleNext}
-                        className="navy-blue hover:text-[#960000] transition-all duration-300 cursor-pointer disabled:opacity-50 disabled:cursor-default disabled:text-[#000000]"
+                        className="navy-blue hover:text-white bg-[#960000] transition-all duration-300 cursor-pointer disabled:opacity-50 disabled:cursor-default disabled:text-[#000000]"
                         disabled={startIndex + itemsPerPage >= productGroup.length}
                         >
-                        <MdKeyboardArrowRight size={32} className="border border-zinc-400 rounded bg-white shadow-sm"/>
+                        <MdKeyboardArrowRight size={32} className="border border-zinc-200 rounded bg-white shadow-sm"/>
                     </button>
                 </div>
             </div>
